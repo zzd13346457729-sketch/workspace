f71e9e35a2eaa626a6327bdcca18efc063198e0b
zzd13346457729 <zzd13346457729@gmail.com>
2026-09-22 16:11:59 +0800

    refactor: 复盘存储模块并重构主程序的菜单与记录管理逻辑

--- py-learning/learning-tracker/v0.py
+++ py-learning/learning-tracker/v0.py
@@ -1,33 +1,53 @@
 from storage import load_records
 from storage import save_records
 
-def input_record():
-    record={}
-    record["subject"]=input("请输入学科：").strip()
-    if not record["subject"]:
-        print("输入不能为空，请重试")
-        return None#
+def add_record(records):
     while True:
-        record["minutes"]=input("请输入时间(min)：").strip()
-        if not record["minutes"]:
-            print("输入不能为空/0，请重试")
-            continue
-        try:
-            record["minutes"]=int(record["minutes"])
-        except ValueError:
-            print("数据类型错误，请输入整数")
-            continue
-        if record["minutes"] <= 0:#
-            print("学习时间必须大于 0，请重试")
-            continue
-        break#
-    record["content"]=input("请输入内容").strip()
-    if not record["content"]:
+        record={}
+        record["subject"]=input("请输入学科：").strip()
+        if not record["subject"].lower().strip():
             print("输入不能为空，请重试")
-            return None#
-    return record##
+            continue
+        while True:
+            record["minutes"]=input("请输入时间(min)：").strip()
+            if not record["minutes"]:
+                print("输入不能为空，请重试")
+                continue
+            try:
+                record["minutes"]=int(record["minutes"])
+            except ValueError:
+                print("数据类型错误，请输入整数")
+                continue
+            if record["minutes"] <= 0:#
+                print("学习时间必须大于 0，请重试")
+                continue
+            break#
+        while True:
+            record["content"]=input("请输入内容").strip()
+            if not record["content"]:
+                    print("输入不能为空，请重试")
+                    continue
+            break
+        new_records = records + [record]
+        if not save_records(new_records):
+            print("本条记录未保存，本次录入结束")
+            break
+        records = new_records
+        while True:
+            ys=input("是否退出记录？请输入yes or no").strip().lower()
+            #if not ys=="yes" or ys=="no":
+            if ys!="yes" and ys!="no":
+                print("输入错误，请重试")
+                continue
+            break
+        if ys=="yes":
+            break
+    return records
 
 def print_records(records):
+     if not records:
+        print("暂无学习记录")
+        return
      for i in records:
           print(i)
 
@@ -38,56 +58,57 @@ def showcase_subject_time(records):
             ####超级重要！
     return total_dic
 
+def total_time(records):
+    total=0
+    for record in records:
+        total+=record["minutes"]
+    return total
+
+def find_record(records):
+    subject=input("请输入要查询的科目：").lower().strip()
+    s=0
+    for i in records:
+        if i["subject"].lower().strip()==subject:
+            print(i)
+            s+=1
+    if s==0:
+        print("未找到该科目记录")
+
+def show_menu():
+    print("\n===== 学习记录管理系统 =====")
+    print("1. 添加学习记录")
+    print("2. 显示全部")
+    print("3. 查询记录")
+    print("4. 查看总时间与科目时间")
+    print("0. 退出程序")
+
 def main():
     #record={}
     records=load_records()
     if records is None:
         print("读取失败，请检查数据文件")
         return
-    total=0
-    for record in records:
-        total += record["minutes"]
-    ysys=0
-        #record={}
     while True:
-            ys=input("是否进入记录？请输入yes or no").strip().lower()
-            #if not ys=="yes" or ys=="no":
-            if ys!="yes" and ys!="no":
-                print("输入错误，请重试")
-                continue
-            else:
-                break
-    if ys=="yes":
-         ysys+=1
-    while ysys:
-        record=input_record()
-        if record is None:
-             continue
-        new_records = records + [record]
-        if not save_records(new_records):
-            print("本条记录未保存，本次录入结束")
-            break
-        records = new_records
-        total += record["minutes"]
-        while True:
-            ys=input("是否退出记录？请输入yes or no").strip().lower()
-            #if not ys=="yes" or ys=="no":
-            if ys!="yes" and ys!="no":
-                print("输入错误，请重试")
-                continue
-            else:
-                break
-        if ys=="yes":
+        show_menu()
+        select=input("请输入:").strip()
+        if select=="1":
+            records=add_record(records)
+        elif select=="2":
+            print_records(records)
+        elif select=="3":
+            find_record(records)
+        elif select=="4":
+            total=total_time(records)
+            print(f"总学习时间：{total}min")
+            total_dic=showcase_subject_time(records)
+            print(f"按科目分类：")
+            for subject, minutes in total_dic.items():
+                print(f"{subject}：{minutes} 分钟")
+        elif select=="0":
+            print("已退出")
             break
         else:
-            continue
-    print_records(records)
-    print(f"总学习时间：{total}min")
-    total_dic=showcase_subject_time(records)
-    print(f"按科目分类：")
-    for subject, minutes in total_dic.items():
-        print(f"{subject}：{minutes} 分钟")
-    ysys=0
+            print("输入错误，请重试，输入0-4")
 
 if __name__ == "__main__":
     main()
